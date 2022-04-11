395955ed1cedaa16147a55af3541b9f72c6989c5
Abdulrahman Alabduljabbar <ajbrsa@gmail.com>
2022-04-11 13:56:38 -0400

    Update BERTClassification.py

--- BERTClassification.py
+++ BERTClassification.py
@@ -1,4 +1,8 @@
-#pip3 install ktrain
+#Install ktrain using the following command
+#pip install ktrain
+
+
+
 
 import ktrain
 from ktrain import text
@@ -6,6 +10,10 @@ from sklearn.model_selection import train_test_split
 import pickle
 import numpy as np
 
+# Download all predictors from the following link:
+# https://drive.google.com/drive/folders/1vzGQSZGBqbbFDf_iA4KmM3-3svWvXNv7
+    
+    
 for cat_number in range(9):
     all_paragraphs = pickle.load(open("Paragraphs.pkl", "rb"))
     reloaded_predictor = ktrain.load_predictor("predictor_cat"+str(cat_number+1))
